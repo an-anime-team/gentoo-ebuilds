265f69c9ea3bd533bf7ebadd1a2fc12cf4886c74
JohnTheCoolingFan <ivan8215145640@gmail.com>
2024-09-01 21:40:14 +0300

    Change how ebuild is tested

---
+++ .github/workflow-utils/add-repo.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+OVERLAY_SOURCE=$1
+OVERLAY_NAME=$(head -z -n 1 $1/profiles/repo_name)
+
+cp -r $1 "/var/db/repos/${OVERLAY_NAME}"
+
+cat << EOF
+[${OVERLAY_NAME}]
+location = /var/db/repos/${OVERLAY_NAME}
+EOF > /etc/portage/repos.conf/${OVERLAY_NAME}.conf
