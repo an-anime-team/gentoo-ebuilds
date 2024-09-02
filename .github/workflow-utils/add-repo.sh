4af961ab96de7053586bb6d468ab0e87709b663a
JohnTheCoolingFan <ivan8215145640@gmail.com>
2024-09-02 22:47:36 +0300

    Workflow and script fixes

--- .github/workflow-utils/add-repo.sh
+++ .github/workflow-utils/add-repo.sh
@@ -5,7 +5,7 @@ OVERLAY_NAME=$(head -z -n 1 $1/profiles/repo_name)
 
 cp -r $1 "/var/db/repos/${OVERLAY_NAME}"
 
-cat << EOF
+cat << EOF > /etc/portage/repos.conf/${OVERLAY_NAME}.conf
 [${OVERLAY_NAME}]
 location = /var/db/repos/${OVERLAY_NAME}
-EOF > /etc/portage/repos.conf/${OVERLAY_NAME}.conf
+EOF
